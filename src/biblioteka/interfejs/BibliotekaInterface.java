21dc4354003c3d16417ce77099841732e1bd8fec
markovical <aca997@gmail.com>
2021-03-15 15:44:19 +0100

    Dokumentovana je BibliotekaInterface

--- src/biblioteka/interfejs/BibliotekaInterface.java
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -5,7 +5,24 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+
+/**
+ * 
+ * Interfejs koji predstavlja biblioteku.
+ * 
+ * @author Aleksandar Markovic
+ *
+ */
 public interface BibliotekaInterface {
+	
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 *
+	 * @param knjiga Knjiga koja se dodaje.
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta knjiga null
+	 * @throws java.lang.RuntimeException ako uneta knjiga vec postoji
+	 */
 
 	public void dodajKnjigu(Knjiga knjiga);
 	
